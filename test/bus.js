cc016ef80e730cf0bac4db237e149601bb4504a7
Jon Moss <me@jonathanmoss.me>
2022-05-21 14:03:18 -0400

    Properly handle stops with no times

--- test/bus.js
+++ test/bus.js
@@ -28,30 +28,35 @@ tap.test('correctly sends bus response if success', (t) => {
         buses: [
           {
             destination: 'Lawrenceville',
+            error: false,
             route: '93',
             status: 'DUE',
             stopId: 2573,
           },
           {
             destination: 'Negley',
+            error: false,
             route: '71A',
             status: '9 minutes',
             stopId: 2573,
           },
           {
             destination: 'Point Breeze',
+            error: false,
             route: '71C',
             status: '13 minutes',
             stopId: 2573,
           },
           {
             destination: 'Negley',
+            error: false,
             route: '71A',
             status: '24 minutes',
             stopId: 2573,
           },
           {
             destination: 'North Side Via Polish Hill',
+            error: false,
             route: '54',
             status: '25 minutes',
             stopId: 2573,
@@ -64,24 +69,28 @@ tap.test('correctly sends bus response if success', (t) => {
         buses: [
           {
             destination: 'Downtown',
+            error: false,
             route: '71C',
             status: '7 minutes',
             stopId: 2633,
           },
           {
             destination: 'South Side to Bon Air',
+            error: false,
             route: '54',
             status: '7 minutes',
             stopId: 2633,
           },
           {
             destination: 'Hazelwood',
+            error: false,
             route: '93',
             status: '26 minutes',
             stopId: 2633,
           },
           {
             destination: 'South Side - SHJ',
+            error: false,
             route: '54',
             status: '28 minutes',
             stopId: 2633,
@@ -94,18 +103,21 @@ tap.test('correctly sends bus response if success', (t) => {
         buses: [
           {
             destination: 'Downtown - North Shore',
+            error: false,
             route: 'SLVR',
             status: '3 minutes',
             stopId: 99995,
           },
           {
             destination: 'Downtown - North Shore',
+            error: false,
             route: 'BLUE',
             status: '21 minutes',
             stopId: 99995,
           },
           {
             destination: 'Downtown - North Shore',
+            error: false,
             route: 'RED',
             status: '21 minutes',
             stopId: 99995,
@@ -118,17 +130,75 @@ tap.test('correctly sends bus response if success', (t) => {
         buses: [
           {
             destination: 'South Hills Village ',
+            error: false,
             route: 'RED',
             status: '14 minutes',
             stopId: 98881,
           },
           {
             destination: 'Library',
+            error: false,
             route: 'SLVR',
             status: '19 minutes',
             stopId: 98881,
           },
         ],
+        name: 'North Side S',
+        stopId: 98881,
+      },
+    ];
+
+    t.strictSame(JSON.parse(res.body), expectedResponse);
+    t.end();
+  });
+});
+
+tap.test('correctly sends bus response if success', (t) => {
+  const busSuccessFixture = readFileSync('./test/bus_success_notimes.json').toString();
+  mock.onGet(busUrl).reply(200, busSuccessFixture);
+
+  const railSuccessFixture = readFileSync('./test/rail_success_notimes.json').toString();
+  mock.onGet(railUrl).reply(200, railSuccessFixture);
+
+  server.inject({
+    method: 'GET',
+    url: '/api/bus',
+  }, (err, res) => {
+    t.equal(200, res.statusCode);
+
+    const expectedResponse = [
+      {
+        buses: [
+          {
+            destination: 'South Side - SHJ',
+            error: false,
+            route: '54',
+            status: 'DUE',
+            stopId: 2633,
+          },
+          {
+            destination: 'South Side to Bon Air',
+            error: false,
+            route: '54',
+            status: '28 minutes',
+            stopId: 2633,
+          },
+        ],
+        name: 'Craig & Centre DT',
+        stopId: 2633,
+      },
+      {
+        buses: [],
+        name: 'Craig & Centre EE',
+        stopId: 2573,
+      },
+      {
+        buses: [],
+        name: 'Steel Plaza N',
+        stopId: 99995,
+      },
+      {
+        buses: [],
         name: 'North Side S',
         stopId: 98881,
       },
